5a8fb59690ba0d573496742879730e81d4c3354e
Michael Schmidt <michael.schmidt.42@t-online.de>
2018-02-15 22:46:20 +0100

    support for hidden files

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -34,3 +34,5 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
 root(ERROR, ['STDOUT'])
+
+logger 'mschmidt3', DEBUG, ['STDOUT'] , false
--- grails-app/services/mschmidt3/LogViewerService.groovy
+++ grails-app/services/mschmidt3/LogViewerService.groovy
@@ -5,12 +5,18 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class LogViewerService {
     String logDirectory
+    ArrayList<String> hideFiles
 
     def listFiles() {
-        log.debug("listFiles ${logDirectory}")
+        log.debug("listFiles ${logDirectory}, ${hideFiles}")
+
         def filelist = []
         new File(logDirectory).eachFile {
-            filelist << it.name
+            if( hideFiles.contains( it.name ) ){
+                log.debug "file '$it' is hidden"
+            } else {
+                filelist << it.name
+            }
         }
 
         return filelist
@@ -18,6 +24,9 @@ class LogViewerService {
 
     String getContent(String fname){
         String result = "file '${fname}' not found"
+
+        if( hideFiles.contains( fname ) ){ return result }
+
         File logfile = new File(logDirectory + '/'+ fname)
         if(logfile.exists()){
             result = logfile.text
